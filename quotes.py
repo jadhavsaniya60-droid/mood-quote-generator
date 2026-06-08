89ba58dac18c24c66005ea5ee3da522983bcdb42
Saniya Uddhav Jadhav <jadhavsaniya60@gmail.com>
2026-06-08 09:55:42 +0530

    Create quotes.py

---
+++ quotes.py
@@ -0,0 +1,22 @@
+quotes = {
+    "happy": [
+        "Happiness is a choice.",
+        "Keep smiling, brighter days are ahead.",
+        "Joy shared is joy doubled."
+    ],
+    "sad": [
+        "Every storm runs out of rain.",
+        "Tough times never last, but tough people do.",
+        "You are stronger than you think."
+    ],
+    "motivated": [
+        "Dream big. Start small. Act now.",
+        "Success is the sum of small efforts.",
+        "Believe in yourself."
+    ],
+    "stressed": [
+        "Take a deep breath and keep going.",
+        "One step at a time.",
+        "You can handle this."
+    ]
+}
